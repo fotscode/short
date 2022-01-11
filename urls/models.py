ae4a3868689fcb18bb11f50949ce432905716726
fots <facundotomatis@gmail.com>
2022-01-10 22:03:54 -0300

    formatting

--- urls/models.py
+++ urls/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class URL(models.Model):
     def __str__(self):
         return self.url_short
+
     url_text = models.CharField(max_length=390)
-    url_short = models.CharField(max_length=10,unique=True, null=False, blank=False)
-    pub_date = models.DateTimeField('date published')
+    url_short = models.CharField(max_length=10, unique=True, null=False, blank=False)
+    pub_date = models.DateTimeField("date published")
