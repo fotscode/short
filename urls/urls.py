21058ba35bd9aad69c6014d458bf1f618cc49aec
fots <facundotomatis@gmail.com>
2022-01-10 22:04:20 -0300

    added detail route, view and controller

--- urls/urls.py
+++ urls/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name='urls'
+app_name = "urls"
 urlpatterns = [
-     path('', views.index, name='index'),
-     path('new/', views.new, name='new'),
-     path('<str:short>/', views.redirect, name='redirect'),
-#    path('<str:url_short>/detail/', views.DetailView.as_view(), name='detail'),
+    path("", views.index, name="index"),
+    path("new/", views.new, name="new"),
+    path("<str:short>/", views.redirect, name="redirect"),
+    path('<str:short>/detail/', views.detail, name='detail'),
 ]
